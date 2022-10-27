61f799f154f1bea30333ba602128b9a81b733ccb
blazej-smorawski <blazej.smorawski@gmail.com>
2022-10-27 18:26:41 +0200

    Make code with users file an argument

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-#/bin/python
+#!/usr/bin/env python3
 
 # Main entrypoint of runtime microservice
 
 from src.runtime import Runtime
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog = 'LNX-Runtime',
+                    description = 'Create game environment and run users code in it',
+                    epilog = 'Owner: blazej.smorawski@gmail.com')
+parser.add_argument('code_file_path')
+
+args = parser.parse_args()    
 
 if __name__ == '__main__':
-     with open('usr/test-agent.py', 'r') as code_file:
+     with open(args.code_file_path, 'r') as code_file:
           code = code_file.read()
           runtime = Runtime(code)
           runtime.run()
