f877ac1dc596b8b80d36920b8ef2db5c08c1b295
FilipSzweda <filip.szweda1@gmail.com>
2022-12-05 20:51:13 +0100

    Change to transitions lib

--- src/objects/npc.py
+++ src/objects/npc.py
@@ -1,13 +1,13 @@
 import random
 
-from statemachine import StateMachine, State
+from transitions import Machine, State
 
 from src.common.point import Point
 from src.objects.object import Object
 from src.common.serializable import Properties
 
 
-class NPC(Object, StateMachine):
+class NPC(Object):
     """
     Simple agent existing in a `scene`.
     """
@@ -15,26 +15,28 @@ class NPC(Object, StateMachine):
     properties: Properties
     scene: 'Scene'
     walkable: bool
+    machine: Machine
 
-    idle_state = State('Idle', initial=True)
-    move_state = State('Move')
-    cycle = idle_state.to(move_state) | move_state.to(idle_state)
+    states = [State(name='idle', on_enter=['idle']), State(name='moving', on_enter=['move'])]
 
     def __init__(self, scene: 'Scene', position: Point) -> None:
         super().__init__(position, scene)
+        self.machine = Machine(model=self, states=NPC.states, initial='idle')
+        self.machine.add_ordered_transitions(loop=True)
 
-    def on_enter_move(self):
-        neighboring_points = [self.position + Point(1, 0), self.position - Point(1, 0), self.position + Point(0, 1),
-                              self.position - Point(0, 1)]
+    def move(self):
+        neighboring_points = [self.properties.position + Point(1, 0), self.properties.position + Point(-1, 0),
+                              self.properties.position + Point(0, 1),
+                              self.properties.position + Point(0, -1)]
         walkable_fields = [point for point in neighboring_points if self.scene.is_walkable_field(point)]
         if walkable_fields:
-            self.scene.move_object(random.choice(walkable_fields))
+            self.scene.move_object(self, random.choice(walkable_fields))
 
-    def on_enter_idle(self):
+    def idle(self):
         pass
 
     def tick(self) -> None:
         """
         Called on an `Object`, so it can perform some actions
         """
-        self.cycle()
+        self.next_state()
